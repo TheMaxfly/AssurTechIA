30ed8833ab0557e1c83ff60d015ab40c1ce1dc6f
Leo <leogaallus@gmail.com>
2025-01-24 09:21:50 +0100

    logout

--- AssurtechIA/authentication/urls.py
+++ AssurtechIA/authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginPageView, RegistrationPageView,profilView
+from .views import LoginPageView, RegistrationPageView, LogoutView, profilView
 from authentication.views import HomeView
 
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('login/', LoginPageView.as_view(), name='login'),
     path('registration/',RegistrationPageView.as_view(), name='registration'),
     path('profil/',profilView,name='profil'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path("", HomeView.as_view(), name="home")
+
 ]
